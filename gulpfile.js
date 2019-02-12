e0a8566976119169e5c54702d3e613400984e643
Dominic Letz <dominicletz@exosite.com>
2019-02-12 16:01:14 +0800

    Commit moved original images to resources

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch(['_scss/*.scss', '_scss/*/*.scss'], ['sass']);
-    gulp.watch(['*.html', '_includes/*.html', '_includes/*/*.html', '_layouts/*.html', '_layouts/*/*.html', '_posts/*', 'js/*.js', 'images/*'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '_includes/*.html', '_includes/*/*.html', '_layouts/*.html', '_layouts/*/*.html', '_posts/*', 'js/*.js', 'images/*', 'blog/*'], ['jekyll-rebuild']);
 });
 
 /**
