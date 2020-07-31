af35a34351466386491188d2b1b92fedb56f03b5
Boncho Valkov <bonchovylkov@abv.bg>
2020-07-31 16:50:04 +0300

    Put prenet.scss in separate file

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task('sass', function () {
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
         .pipe(gulp.dest('_site/css'))
         .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('prenet'));
+        .pipe(gulp.dest('css'));
 });
 
 /**
