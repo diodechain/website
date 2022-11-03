e49119c76276eb70cb56e6f435277f43d965f546
Dominic Letz <dominic@diode.io>
2022-11-03 10:15:32 +0100

    Updated vesting to Metamask 10.x

---
+++ vesting/precache-manifest.dc6d5d5e578dcada44d713421a4255ff.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bb84ca4b8899c7578850e632cb7756",
+    "url": "/vesting/index.html"
+  },
+  {
+    "revision": "e3d2be69c947492770bc",
+    "url": "/vesting/static/css/2.37c3a404.chunk.css"
+  },
+  {
+    "revision": "62aeb16043c7445dd31f",
+    "url": "/vesting/static/css/main.f5ede383.chunk.css"
+  },
+  {
+    "revision": "e3d2be69c947492770bc",
+    "url": "/vesting/static/js/2.a6999a68.chunk.js"
+  },
+  {
+    "revision": "798a0756542dfd116fb72eeec2fa910d",
+    "url": "/vesting/static/js/2.a6999a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62aeb16043c7445dd31f",
+    "url": "/vesting/static/js/main.45a8dc84.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/vesting/static/js/main.45a8dc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14ffbe1b5f1c34c5926e",
+    "url": "/vesting/static/js/runtime-main.98fda636.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/vesting/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/vesting/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/vesting/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/vesting/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/vesting/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- vesting/service-worker.js
+++ vesting/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vesting/precache-manifest.f22bddc5ab658156f5979f326ace6b3e.js"
+  "/vesting/precache-manifest.dc6d5d5e578dcada44d713421a4255ff.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vesting/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vesting/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
