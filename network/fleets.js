10fc66079ec462a52e41068c9a798679adc20136
hansr <hansrempel@gmail.com>
2025-08-12 15:37:54 -0700

    updating fleets

--- network/fleets.js
+++ network/fleets.js
@@ -19,7 +19,6 @@ const Fleetlist = {
         "name": "Diode Collab",
         "icon": "img/collab.png"
     },
-    
     "0x260e9F2aB47Bb0934BF84E5D33ee81F9EBe27D83": {
         "name": "Exosite",
         "icon": "img/exosite.png"
@@ -27,7 +26,36 @@ const Fleetlist = {
     "0x8c2D2Cf27f39e8dF83ad1a30cBBCC6d9cB410f89": {
         "name": "Ampscale",
         "icon": "img/ampscale.png"
+    },
+    "0xB2F558d0e0A64e9Cf86CD314FeEaf358302d2e5e": {
+        "name": "Insight Software",
+        "icon": "img/insight.png"
+    },
+    "0xBAe412cA6A63cc174A4d9296CDe801a889aa1871": {
+        "name": "Mirai Window",
+        "icon": "img/mirai.png"
+    },
+    "0xdBAa6d4f29CcB5543fC2E9821E58c2dfBE4A8c59": {
+        "name": "The New Internet",
+        "icon": "img/newinternet.png"
+    },
+    "0x8B02AaF758a4ED3210081d4226a7E6683335E5eb": {
+        "name": "Silicon Labs",
+        "icon": "img/silabs.png" 
+    },
+    "0xE1AedbeaCEe8f6Ee075656fabE005021c5697A50": {
+        "name": "Renesas",
+        "icon": "img/renesas.png" 
+    },
+    "0xB13A36f5e8fd91Aa4851787507B28C9ED9f879F2": {
+        "name": "NXP",
+        "icon": "img/nxp.png" 
+    },
+    "0x4A97503cE7840Eb6c9c6C44f90227B90a629C8e0": {
+        "name": "Solertiae",
+        "icon": "img/solertiae.png" 
     }
+
 }
 
 function getFleet(address) {
